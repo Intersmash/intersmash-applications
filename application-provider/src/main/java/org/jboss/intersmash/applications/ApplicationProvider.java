2a468a4a23d4ef0e3302eb1b13088cf6b1e662f3
Tommaso Borgato <tborgato@redhat.com>
2026-02-04 11:00:52 +0100

    [issue-77] Create an application to test using jms-bridge to forward messages from local queues to a remote AMQ Broker

--- application-provider/src/main/java/org/jboss/intersmash/applications/ApplicationProvider.java
+++ application-provider/src/main/java/org/jboss/intersmash/applications/ApplicationProvider.java
@@ -32,6 +32,7 @@ public class ApplicationProvider {
 	static final String WILDFLY_WEB_CACHE_OFFLOAD_INFINISPAN_DEPLOYMENT = "wildfly-web-cache-offload-infinispan";
 	static final String WILDFLY_DISTRIBUTED_SESSIONS_INFINISPAN_DEPLOYMENT = "wildfly-distributed-sessions-infinispan";
 	static final String WILDFLY_ACTIVEMQ_ARTEMIS_BROKER_SSL_DEPLOYMENT = "wildfly-activemq-artemis-ssl";
+	static final String WILDFLY_ACTIVEMQ_ARTEMIS_BROKER_JMS_BRIDGE_DEPLOYMENT = "wildfly-activemq-artemis-jms-bridge";
 	static final String WILDFLY_KEYCLOAK_SAML_ADAPTER_DEPLOYMENT = "wildfly-keycloak-saml-adapter";
 	static final String WILDFLY_KEYCLOAK_SAML_ADAPTER_EJB_DEPLOYMENT = "wildfly-keycloak-saml-adapter-ejb";
 	static final String WILDFLY_DEPLOYMENT_ARTIFACT_PACKAGING_WAR = "war";
@@ -70,6 +71,18 @@ public class ApplicationProvider {
 	 */
 	public static Path wildflyActiveMQArtemisSslProvisionedServerPath() {
 		return findApplicationDirectory("wildfly", "activemq-artemis-ssl", "target", "server");
+	}
+
+	/**
+	 * Provides access to a filesystem directory containing a server provisioned by
+	 * the WildFly/JBoss EAP 8.x Maven plugin, which is generated from the
+	 * {@code wildfly-activemq-artemis-jms-bridge} application
+	 *
+	 * @return {@link Path} instance that identifies the directory containing the
+	 *         WildFly/JBoss EAP 8.x provisioned server.
+	 */
+	public static Path wildflyActiveMQArtemisJmsBridgeProvisionedServerPath() {
+		return findApplicationDirectory("wildfly", "activemq-artemis-jms-bridge", "target", "server");
 	}
 
 	/**
@@ -213,6 +226,26 @@ public class ApplicationProvider {
 			file = ArtifactProvider.resolveArtifact(ApplicationConfigurationProperties.groupID(),
 					WILDFLY_ACTIVEMQ_ARTEMIS_BROKER_SSL_DEPLOYMENT,
 					ApplicationConfigurationProperties.version(), WILDFLY_DEPLOYMENT_ARTIFACT_PACKAGING_WAR, null)
+					.toPath();
+		} catch (SettingsBuildingException | ArtifactResolutionException e) {
+			throw new RuntimeException("Can not get artifact", e);
+		}
+		return file;
+	}
+
+	/**
+	 * Provides access to a WAR deployment containing the
+	 * {@code wildfly-activemq-artemis-jms-bridge} application
+	 *
+	 * @return {@link Path} instance that identifies the WAR artifact containing the
+	 *         {@code wildfly-activemq-artemis-jms-bridge} application.
+	 */
+	public static Path wildflyActiveMQArtemisJmsBridgeDeploymentPath() {
+		Path file = null;
+		try {
+			file = ArtifactProvider.resolveArtifact(ApplicationConfigurationProperties.groupID(),
+							WILDFLY_ACTIVEMQ_ARTEMIS_BROKER_JMS_BRIDGE_DEPLOYMENT,
+							ApplicationConfigurationProperties.version(), WILDFLY_DEPLOYMENT_ARTIFACT_PACKAGING_WAR, null)
 					.toPath();
 		} catch (SettingsBuildingException | ArtifactResolutionException e) {
 			throw new RuntimeException("Can not get artifact", e);
