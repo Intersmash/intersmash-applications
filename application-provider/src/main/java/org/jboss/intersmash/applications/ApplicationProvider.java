63bbb78ab2b01ccb9cdbad6b8958f7d87a2d24f9
Tommaso Borgato <tborgato@redhat.com>
2026-03-18 14:51:41 +0100

    Eap8WithPerConnectorSecuredAmqStreamsTests -> WildflyS2iPerConnectorSecuredKafkaIT Eap8BootableJarWithPerConnectorSecuredAmqStreamsTests -> WildflyBootableJarPerConnectorSecuredKafkaIT Eap8BootableJarWithGloballySecuredAmqStreamsTests -> WildflyBootableJarGloballySecuredKafkaIT

--- application-provider/src/main/java/org/jboss/intersmash/applications/ApplicationProvider.java
+++ application-provider/src/main/java/org/jboss/intersmash/applications/ApplicationProvider.java
@@ -35,6 +35,7 @@ public class ApplicationProvider {
 	static final String WILDFLY_KEYCLOAK_SAML_ADAPTER_DEPLOYMENT = "wildfly-keycloak-saml-adapter";
 	static final String WILDFLY_KEYCLOAK_SAML_ADAPTER_EJB_DEPLOYMENT = "wildfly-keycloak-saml-adapter-ejb";
 	static final String WILDFLY_KEYCLOAK_SAML_ADAPTER_EJB_BOOTABLE_JAR_DEPLOYMENT = "wildfly-keycloak-saml-adapter-ejb-bootable-jar";
+	static final String WILDFLY_KAFKA_APPLICATION_DEPLOYMENT = "wildfly-kafka-application";
 	static final String WILDFLY_DEPLOYMENT_ARTIFACT_PACKAGING_WAR = "war";
 	static final String WILDFLY_DEPLOYMENT_ARTIFACT_PACKAGING_JAR = "jar";
 
@@ -274,6 +275,26 @@ public class ApplicationProvider {
 		try {
 			file = ArtifactProvider.resolveArtifact(ApplicationConfigurationProperties.groupID(),
 					WILDFLY_KEYCLOAK_SAML_ADAPTER_EJB_BOOTABLE_JAR_DEPLOYMENT,
+					ApplicationConfigurationProperties.version(), WILDFLY_DEPLOYMENT_ARTIFACT_PACKAGING_JAR, "bootable")
+					.toPath();
+		} catch (SettingsBuildingException | ArtifactResolutionException e) {
+			throw new RuntimeException("Can not get artifact", e);
+		}
+		return file;
+	}
+
+	/**
+	 * Provides access to a bootable JAR deployment containing the
+	 * {@code wildfly-kafka-application} application
+	 *
+	 * @return {@link Path} instance that identifies the bootable JAR artifact containing the
+	 *         {@code wildfly-kafka-application} application.
+	 */
+	public static Path wildflyKafkaApplicationBootableJarDeploymentPath() {
+		Path file = null;
+		try {
+			file = ArtifactProvider.resolveArtifact(ApplicationConfigurationProperties.groupID(),
+					WILDFLY_KAFKA_APPLICATION_DEPLOYMENT,
 					ApplicationConfigurationProperties.version(), WILDFLY_DEPLOYMENT_ARTIFACT_PACKAGING_JAR, "bootable")
 					.toPath();
 		} catch (SettingsBuildingException | ArtifactResolutionException e) {
