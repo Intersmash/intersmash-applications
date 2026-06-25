b67d2abed6e16c6b3337b20f654eb73735612207
Tommaso Borgato <tborgato@redhat.com>
2026-06-25 17:50:31 +0200

    [issues-87] - Adding the Infinispan Distributed Timers application - logging

--- wildfly/timer-expiration-store/src/main/java/org/jboss/intersmash/applications/wildfly/timer/expiration/store/TimerExpirationStoreResource.java
+++ wildfly/timer-expiration-store/src/main/java/org/jboss/intersmash/applications/wildfly/timer/expiration/store/TimerExpirationStoreResource.java
@@ -11,12 +11,16 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import java.time.Instant;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * A Jakarta RESTful Web Services resource class, that exposes the EJB Timer Expiration Store REST APIs.
  */
 @Path("/timer")
 public class TimerExpirationStoreResource {
+
+	protected static final Logger log = Logger.getLogger(TimerExpirationStoreResource.class.getName());
 
 	@EJB
 	TimerExpirationStore<MonitoredTimerExpiration> timerExpirationStore;
@@ -26,6 +30,7 @@ public class TimerExpirationStoreResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getTimerExpiration(@PathParam("id") Long id) {
 		final MonitoredTimerExpiration timerExpiration = timerExpirationStore.getById(id);
+		log.log(Level.INFO, "MonitoredTimerExpiration: {0}", timerExpiration.toString());
 		return Response.ok(timerExpiration).build();
 	}
 
@@ -33,6 +38,7 @@ public class TimerExpirationStoreResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getTimerExpirations() {
 		final List<MonitoredTimerExpiration> timerExpirations = timerExpirationStore.getAll();
+		log.log(Level.INFO, "List<MonitoredTimerExpiration>: {0}", timerExpirations.toArray());
 		return Response.ok(timerExpirations).build();
 	}
 
@@ -41,6 +47,7 @@ public class TimerExpirationStoreResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getTimerExpirations(@QueryParam("from") Instant from, @QueryParam("to") Instant to) {
 		final List<MonitoredTimerExpiration> timerExpirations = timerExpirationStore.getInTimeRange(from, to);
+		log.log(Level.INFO, "List<MonitoredTimerExpiration>: {0}", timerExpirations.toArray());
 		return Response.ok(timerExpirations).build();
 	}
 
@@ -48,6 +55,7 @@ public class TimerExpirationStoreResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response deleteTimerExpirations() {
 		final int affectedTimerExpirations = timerExpirationStore.deleteAll();
+		log.log(Level.INFO, "Deleted Timer Expirations: {0}", affectedTimerExpirations);
 		return Response.ok(affectedTimerExpirations).build();
 	}
 }
